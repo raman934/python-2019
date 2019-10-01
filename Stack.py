7b5142268ecc60e1b4a136909c1fcdc0ade7e50a
Raman Gandhi <ramangandhi392000@gmail.com>
2019-10-01 12:59:03 +0530

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,61 +1,22 @@
-# RG
-# 11.2
-
-
-class Node:
-
-    def __init__(self, val):
-        self.val = val
-        self.next = None
-
-
 class Stack:
 
     def __init__(self):
-        self.start = None
-        self.end = None
-        self.count = 0
+        self.data = []
 
     def push(self, val):
-        new_node = Node(val)
-        if self.start is None:
-            self.start = new_node
-            self.end = new_node
-        else:
-            self.end.next = new_node
-            self.end = new_node
-        self.count += 1
+        self.data.append(val)
 
     def pop(self):
-        if self.start is None:
-            return
-        elif self.start == self.end:
-            self.start = None
-            self.end = None
-        else:
-            self.end = self.end.next
-        self.count -= 1
+        return self.data.pop()
 
     def get_size(self):
-        return self.count
+        return len(self.data)
 
     def is_empty(self):
-        return self.count == 0
-
-    def front(self):
-        if self.start is None:
-            return None
-        return self.start.val
+        return len(self.data) == 0
 
     def __len__(self):
-        return self.count
+        return len(self.data)
 
     def __str__(self):
-        s = ''
-        current_node = self.start
-        while current_node is not None:
-            s += str(current_node.val)
-            current_node = current_node.next
-            if current_node is not None:
-                s += ', '
-        return s
+        return str(self.data)
